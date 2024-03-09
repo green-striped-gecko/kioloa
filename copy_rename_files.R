3c51f64f81f63c69b0a53f52fd321b62a1bb1ab3
Emily Jayne <ejstringer@live.com.au>
2024-03-10 02:04:51 +1100

    tidying up for windows

---
+++ copy_rename_files.R
@@ -0,0 +1,17 @@
+dir.create(
+  './sessions/'
+)
+
+fromfile <- list.files(pattern = '_code')
+tofile <- sub('_code', '', fromfile)
+
+for (i in 1:length(fromfile)) {
+  file.copy(from = paste0('./',fromfile[i]),
+            to = paste0('./',tofile[i]))
+  
+  # file.rename(from = './sessions/session04.qmd',
+  #             to = paste0('./sessions/session', i,'.qmd'))
+  
+}
+
+
