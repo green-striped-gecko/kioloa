4fa7dec76644059961ced0893f7b6f31081f9ecb
Emily Jayne <ejstringer@live.com.au>
2024-03-10 04:00:59 +1100

    sessions 11 and 12 to render

--- copy_rename_files.R
+++ copy_rename_files.R
@@ -7,7 +7,7 @@ tofile <- sub('_code', '', fromfile)
 
 for (i in 1:length(fromfile)) {
   file.copy(from = paste0('./',fromfile[i]),
-            to = paste0('./',tofile[i]))
+            to = paste0('./',tofile[i]), overwrite = T)
   
   # file.rename(from = './sessions/session04.qmd',
   #             to = paste0('./sessions/session', i,'.qmd'))
